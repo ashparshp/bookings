19eb27560f250ac19bef7a8bfca58004a0555073
Ashparshp <ashparsh.connects@gmail.com>
2025-05-19 10:29:58 +0530

    feat(tests): add unit tests for main package components
    Added tests for:
    - main entry point
    - middleware functions
    - route handlers
    - setup utilities

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -20,33 +20,10 @@ var app config.AppConfig
 var session *scs.SessionManager
 
 func main() {
-
-	// what am I going to put in the session
-	gob.Register(models.Reservation{})
-
-
-	// change this to true when in production
-	app.InProduction = false
-
-	session = scs.New()
-	session.Lifetime = 24 * time.Hour
-	session.Cookie.Persist = true
-	session.Cookie.SameSite = http.SameSiteLaxMode
-	session.Cookie.Secure = app.InProduction
-	app.Session = session
-
-	tc, err := render.CreateTemplateCache()
+	err := run()
 	if err != nil {
-		log.Fatal("Cannot create template cache")
+		log.Fatal(err)
 	}
-
-	app.TemplateCache = tc
-	app.UseCahce = false
-
-	repo := handlers.NewRepo(&app)
-	handlers.NewHandler(repo)
-
-	render.NewTemplates(&app)
 
 	/*
 	http.HandleFunc("/", handlers.Repo.HomePage)
@@ -68,4 +45,36 @@ func main() {
 
 	err = srv.ListenAndServe()
 	log.Fatal(err)
+}
+
+func run() error {
+		// what am I going to put in the session
+	gob.Register(models.Reservation{})
+
+
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("Cannot create template cache")
+		return err
+	}
+
+	app.TemplateCache = tc
+	app.UseCahce = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandler(repo)
+
+	render.NewTemplates(&app)
+
+	return nil
 }
