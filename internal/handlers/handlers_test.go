6b761e3a629090e49807df5f54e8c011e3cce9aa
Ashparshp <ashparsh.connects@gmail.com>
2025-05-19 12:00:35 +0530

    feat(tests): add additional test cases for search availability and make reservation handlers

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
@@ -67,6 +68,38 @@ var theTests = []struct {
 		params: []postData{},
 		expectedStatusCode: http.StatusOK,
 	},
+	{
+		name: "psa",
+		method: "POST",
+		url: "/search-availability",
+		params: []postData{
+			{"start", "2025-10-01"},
+			{"end", "2025-10-02"},
+		},
+		expectedStatusCode: http.StatusOK,
+	},
+	{
+		name: "psa-json",
+		method: "POST",
+		url: "/search-availability-json",
+		params: []postData{
+			{key: "start", value: "2025-10-01"},
+			{key: "end", value: "2025-10-02"},
+		},
+		expectedStatusCode: http.StatusOK,
+	},
+	{
+		name: "mr",
+		method: "POST",
+		url: "/make-reservation",
+		params: []postData{
+			{key: "first_name", value: "ashparsh"},
+			{key: "last_name", value: "pandey"},
+			{key: "email", value: "ashparsh@gmail.com"},
+			{key: "phone", value: "1234567890"},
+		},
+		expectedStatusCode: http.StatusOK,
+	},
 }
 
 func TestHandlers(t *testing.T) {
@@ -86,7 +119,19 @@ func TestHandlers(t *testing.T) {
 				t.Errorf("For %s, expected %d, got %d", e.name, e.expectedStatusCode, res.StatusCode)
 			}
 		} else {
-
+			values := url.Values{}
+			for _, x := range e.params {
+				values.Add(x.key, x.value)
+			}
+			res, err := ts.Client().PostForm(ts.URL+e.url, values)
+			if err != nil {
+				t.Log(err)
+				t.Fatal()
+			}
+			if res.StatusCode != e.expectedStatusCode {
+				t.Errorf("For %s, expected %d, got %d", e.name, e.expectedStatusCode, res.StatusCode)
+			}
+			res.Body.Close()
 		}
 
 	}
