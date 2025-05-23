811056964f6672773b10698ca1dc71a0cab5d631
Ashparshp <ashparsh.connects@gmail.com>
2025-05-23 13:57:31 +0530

    feat(repository): add SearchAvailabilityForAllRooms method to DatabaseRepo interface

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,11 +1,16 @@
 package repository
 
-import "github.com/ashparshp/bookings/internal/models"
+import (
+	"time"
+
+	"github.com/ashparshp/bookings/internal/models"
+)
 type DatabaseRepo interface {
 	AllUsers() bool
 
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(r models.RoomRestriction) error
 	SearchAvailabilityByDatesByRoomID(start, end string, roomID int) (bool, error)
+	SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error)
 }
 
