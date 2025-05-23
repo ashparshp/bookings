40a70df25638d6eb851af40dc173adf9fa419de9
Ashparshp <ashparsh.connects@gmail.com>
2025-05-23 21:40:48 +0530

    feat(handlers): include room ID and reservation dates in availability response

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -245,6 +245,9 @@ func (m *Repository) PostAvailabilityPage (w http.ResponseWriter, r *http.Reques
 type jsonResponse struct {
 	OK      bool   `json:"ok"`
 	Message string `json:"message"`
+	RoomID  string    `json:"room_id"`
+	StartDate string `json:"start_date"`
+	EndDate   string `json:"end_date"`
 }
 
 // AvailabilityJSON handles request for availability and sends JSON response
@@ -279,6 +282,9 @@ func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
 	resp := jsonResponse{
 		OK:      available,
 		Message: "",
+		StartDate: sd,
+		EndDate:   ed,
+		RoomID: strconv.Itoa(roomID),
 	}
 
 	out, err := json.MarshalIndent(resp, "", "     ")
