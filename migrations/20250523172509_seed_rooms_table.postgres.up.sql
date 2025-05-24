08ba6d6225baf76798009be81b4378a28436ef7d
Ashparshp <ashparsh.connects@gmail.com>
2025-05-24 11:05:10 +0530

    feat(migrations): add seed data for rooms and restrictions tables

---
+++ migrations/20250523172509_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO Public.rooms (room_name, created_at, updated_at) Values
+('General''s Quaters', '2023-05-23 23:00:00.000', '2023-05-23 23:00:00.000'),
+('Major''s Suite', '2023-05-23 23:00:00.000', '2023-05-23 23:00:00.000');
