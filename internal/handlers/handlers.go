cc70b3b16a4c0d65bab46d624b16cc95224c4656
Ashparshp <ashparsh.connects@gmail.com>
2025-05-27 22:51:16 +0530

    feat(admin): initialize reservation and block maps for room reservations in calendar

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -672,6 +672,17 @@ func (m *Repository) AdminReservationCalendarPage(w http.ResponseWriter, r *http
 
 	data["rooms"] = rooms
 
+	for _, room := range rooms {
+		reservationMap := make(map[string]int)
+		blockMap := make(map[string]int)
+
+		for d := firstOfMonth; d.After(lastOfMonth) == false; d = d.AddDate(0, 0, 1) {
+			reservationMap[d.Format("2006-01-02")] = 0
+			blockMap[d.Format("2006-01-02")] = 0
+		}
+
+	}
+
 	render.Template(w, r, "admin-reservations-calendar.page.tmpl", &models.TemplateData{
 		StringMap: stringMap,
 		Data: data,
