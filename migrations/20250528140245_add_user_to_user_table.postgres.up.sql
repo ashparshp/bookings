9d66264fc058ecbd34c0b157b73dbdbdeead4d28
Ashparshp <ashparsh.connects@gmail.com>
2025-05-28 19:35:47 +0530

    feat(migrations): add SQL migration to create user table with initial data

---
+++ migrations/20250528140245_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO "public"."users"("first_name","last_name","email","password","access_level","created_at","updated_at")
+VALUES
+(1,E'ashparsh',E'pandey',E'ashparsh@admin.com',E'$2a$12$lMxZd9rMZa.9quC.pNGVZeAzVTCEJ3enTiPCARdts/hFI.90KypJu',3,E'2025-05-26 00:00:00',E'2025-05-26 00:00:00');
